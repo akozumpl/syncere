9a444bd5a74df3de7938557fb712b8dc71ba27a3
Ales Kozumplik <kozumplik@gmail.com>
2023-11-05 22:21:40 +0000

    Show instance for KeyEntry.

--- src/main/scala/cative/syncere/meta/KeyEntry.scala
+++ src/main/scala/cative/syncere/meta/KeyEntry.scala
@@ -1,10 +1,14 @@
 package cative.syncere.meta
 
+import cats.Show
+
 import io.circe.Codec
 
 object KeyEntry {
   type Key = String
   type Tag = String
+
+  given Show[KeyEntry] = Show.show(_.name)
 }
 
 case class KeyEntry(
