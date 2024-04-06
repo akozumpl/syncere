069196cddccbd0efaeb5b71c34d9441cb3b6f04d
Ales Kozumplik <kozumplik@gmail.com>
2024-04-06 10:30:53 +0100

    refactor: retryInfinitely() -> retry().

--- src/test/scala/cative/syncere/packageTest.scala
+++ src/test/scala/cative/syncere/packageTest.scala
@@ -6,6 +6,7 @@ import java.time.Instant
 import scala.concurrent.duration._
 import cats.effect.Ref
 import cats.syntax.monadError.*
+import cats.syntax.option.*
 
 import weaver.SimpleIOSuite
 
@@ -22,12 +23,24 @@ object packageTest extends SimpleIOSuite {
     )
   }
 
-  test("retryInfinitely does just that.") {
-    val thrice = Ref.unsafe(3)
-    val io = thrice
+  test("retry() can retry infinitely.") {
+    val ten = Ref.unsafe(10)
+    val io = ten
       .getAndUpdate(_ - 1)
       .map(_ < 0)
       .ensure(new IOException("the aura of nostalgia"))(identity)
-    retryInfinitely(None, io, 1.millisecond).map(res => expect(res))
+    retry(None, io, 1.millisecond, None).map(res => expect(res))
+  }
+
+  test("retry() will retry given number of times.") {
+    val four = Ref.unsafe(4)
+    val io = four
+      .getAndUpdate(_ - 1)
+      .map(_ < 0)
+      .ensure(new IOException("the aura of nostalgia"))(identity)
+    for {
+      notEnough <- retry(None, io, 1.millisecond, 4.some).attempt
+      enough <- retry(None, io, 1.millisecond, 5.some).attempt
+    } yield expect(enough.isRight && notEnough.isLeft)
   }
 }
